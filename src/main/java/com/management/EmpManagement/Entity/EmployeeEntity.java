9224e7689b3707d9f07027fcd352905d31fc43de
IshikaBangar123 <ishika.bangar@intelliatech.com>
2023-08-21 19:02:09 +0530

    Employee Module.

--- src/main/java/com/management/EmpManagement/Entity/EmployeeEntity.java
+++ src/main/java/com/management/EmpManagement/Entity/EmployeeEntity.java
@@ -1,4 +1,31 @@
 package com.management.EmpManagement.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String empId;
+    private String empName;
+    private Date dateOfBirth;
+    private Date dateOfJoining;
+    private String permanentAddress;
+    private String localAddress;
+
 }
--- src/main/java/com/management/EmpManagement/Service/ServiceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/management/EmpManagement/Service/ServiceImpl/EmployeeServiceImpl.java
@@ -1,6 +1,46 @@
 package com.management.EmpManagement.Service.ServiceImpl;
 
+import com.management.EmpManagement.Dto.EmployeeDto;
+import com.management.EmpManagement.Entity.EmployeeEntity;
+import com.management.EmpManagement.Repo.EmployeeRepo;
 import com.management.EmpManagement.Service.EmployeeService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepo empRepo;
+    @Override
+    public EmployeeEntity saveEmp(EmployeeDto dto) {
+        EmployeeEntity emp = new EmployeeEntity();
+        BeanUtils.copyProperties(dto, emp);
+        return empRepo.save(emp);
+    }
+
+    @Override
+    public EmployeeEntity getByEmpId(Long id) {
+        return empRepo.findById(id).get();
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllEmp() {
+        return empRepo.findAll();
+    }
+
+    @Override
+    public EmployeeEntity updateEmp(Long id, EmployeeDto dto) {
+        EmployeeEntity emp = empRepo.findById(id).get();
+        BeanUtils.copyProperties(dto, emp);
+        return emp;
+    }
+
+    @Override
+    public void deleteEmp(Long id) {
+      empRepo.deleteById(id);
+    }
 }
