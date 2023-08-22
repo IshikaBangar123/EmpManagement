191da47f44f878d31456440a5e2fd86cb746a5d8
sumit-intelliatech <sumit.pawar@intelliatech.com>
2023-08-22 13:50:14 +0530

    "changes"
    This reverts commit 3e532139822081b868622586c5193205df09e0da.

--- src/main/java/com/management/EmpManagement/Service/PartialWfhService.java
+++ src/main/java/com/management/EmpManagement/Service/PartialWfhService.java
@@ -1,6 +1,6 @@
 package com.management.EmpManagement.Service;
 
-import com.management.EmpManagement.Dto.request.HalfDayReqDto;
+
 import com.management.EmpManagement.Dto.request.PartialReqDto;
 import com.management.EmpManagement.Dto.request.WorkFromHomeReqDto;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +11,6 @@ public interface PartialWfhService {
     public ResponseEntity<?> applyForPartial(PartialReqDto partialReqDto);
 
     public ResponseEntity<?> applyForWfh(WorkFromHomeReqDto workFromHomeReqDto);
-    public ResponseEntity<?> applyForHalfDay(HalfDayReqDto halfDayReqDto);
 
 /*
     public ResponseEntity<?> viewLeaveById(Long pwId);
